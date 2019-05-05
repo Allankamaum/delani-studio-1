af13ebd063b7a616e3240e45e51e1b8ed060e0fb
James Mutahi <mutahijames0@gmail.com>
2019-05-05 21:14:29 +0300

    Add Javascript: form validation

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ $(document).ready(function() {
         $(".classAdd4").addClass("mask flex-center");
     });
 });
+
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function(event) {
+    // Validate form for entry
+    var x = document.forms["myForm"]["contact-name"].value;
+    if (x == "") {
+        alert("Name not entered");
+        return false;
+    }
+    var y = document.forms["myForm"]["contact-email"].value;
+    if (y == "") {
+        alert("Email not entered");
+        return false;
+    }
+    var z = document.forms["myForm"]["contact-message"].value;
+    if (z == "") {
+        alert("You have no message to send");
+        return false;
+    }
+}
